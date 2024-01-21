d03ccfb336e72b2cffcd29cd3c678a0bf4017acd
Dylan Langston <dylanlangston@gmail.com>
2024-01-20 21:43:59 -0500

    Update prefetch of dylanlangston.com.wasm

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <svelte:head>
-    <link rel="prefetch" href="/dylanlangston.com.wasm" />
+    <link rel="prefetch" href="dylanlangston.com.wasm" />
 
     <!-- GTAG Partytown 🕶️ -->
 	<script>
