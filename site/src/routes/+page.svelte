4e7ee0700f861b216f6893362fac95377f22b213
Dylan Langston <dylanlangston@gmail.com>
2024-01-22 22:22:31 -0500

    Add mobile detection

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1 class="text-center text-2xl lg:text-6xl font-bold">Hello HTML World</h1>
+<script lang="ts">
+import { Environment } from "$lib/Common";
+import { onMount } from 'svelte';
+import { fade, blur, fly, slide, scale, crossfade } from 'svelte/transition';
+
+let loaded: boolean = false;
+
+onMount(() => loaded = true);
+</script>
+
+{#if loaded}
+<h1 in:fade={{ duration: 300 }} class="text-center text-2xl lg:text-6xl font-bold">Hello {Environment.isMobile ? "Mobile" : "Desktop"} HTML World</h1>
+{/if}
