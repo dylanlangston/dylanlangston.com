da5d9e5a3843532867a801003719492c7d4352f8
Dylan Langston <dylanlangston@gmail.com>
2024-01-22 21:22:01 -0500

    Detect Browser

--- site/src/lib/Common.ts
+++ site/src/lib/Common.ts
@@ -1,5 +1,12 @@
+import { BrowserDetector } from 'browser-dtector';
+
 export class Environment {
     public static get Dev(): boolean {
         return import.meta.env.MODE == 'development';
+    }
+
+    private static detector = new BrowserDetector();
+    public static get isMobile(): boolean {
+        return this.detector.parseUserAgent().isMobile;
     }
 }
