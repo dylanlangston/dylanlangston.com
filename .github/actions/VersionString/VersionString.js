d70c118eab5f66882b31742dee8dd558b2b4db81
Dylan Langston <dylanlangston@gmail.com>
2024-01-23 22:52:28 -0500

    Convert VersionString to JavaScript Action

---
+++ .github/actions/VersionString/VersionString.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+          
+const date = new Date(core.getInput('time').toString());
+
+if (!isNaN(date)) core.setFailed("Failed to set date input time: ${{ inputs.time }}");
+
+const major = 1;
+const minor = parseInt(`${date.getFullYear()-2000}${date.getMonth()}${date.getDate()}`);
+const patch = parseInt(`${date.getHours()}${date.getMinutes()}${date.getSeconds()}`);
+
+core.setOutput('version', `${major}.${minor}.${patch}`);
+core.notice(`Version: ${major}.${minor}.${patch}`);
