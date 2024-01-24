6f38e398233471839d2f23e4f7c125c0edb366d8
Dylan <dylan.langston@gmail.com>
2024-01-24 15:07:32 -0500

    Try to fix summary.js

--- .github/actions/PopulateBuildSummary/main.mjs
+++ .github/actions/PopulateBuildSummary/main.mjs
@@ -1,4 +1,5 @@
-import { summary } from './summary';
+import { summary } from './summary.js';
 
 const version = process.env.INPUT_VERSION;
+summary.
 summary.addHeading(`DylanLangston.com - v${version}`);
