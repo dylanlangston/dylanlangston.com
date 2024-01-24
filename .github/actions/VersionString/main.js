e1863d7236adc56fcdb77020b1aedac3d0c97ef8
Dylan <dylan.langston@gmail.com>
2024-01-24 10:50:32 -0500

    Issue file command and testing error

--- .github/actions/VersionString/main.js
+++ .github/actions/VersionString/main.js
@@ -1,7 +1,15 @@
 const { EOL } = require("os");
 
 const issueCommand = (cmd) => process.stdout.write(`${cmd}${EOL}`);
-const output = (name, value) => issueCommand(`::set-output name=${name}::${value}`);
+const issueFileCommand = (command, message) => {
+    const filePath = process.env[`GITHUB_${command.toUpperCase()}`]
+    if (!filePath) throw new Error(`Unable to find environment variable for file command ${command}`);
+    if (!fs.existsSync(filePath)) throw new Error(`Missing file at path: ${filePath}`);
+  
+    fs.appendFileSync(filePath, `${message}${os.EOL}`, { encoding: 'utf8' });
+}
+
+const output = (name, value) => issueFileCommand("output", `${name}=${value}`);
 const notice = (value) => issueCommand(`::notice::${value}`);
 
 const date = new Date(process.env.INPUT_TIME);
@@ -10,10 +18,10 @@ const major = 1;
 const minor = parseInt(`${date.getFullYear() - 2000}${date.getMonth()}${date.getDate()}`);
 const patch = parseInt(`${date.getHours()}${date.getMinutes()}${date.getSeconds()}`);
 
+throw new Error(`Failed to set date input time: ${time}`);
+
 if (isNaN(minor) || isNaN(patch)) 
 {
-    process.stdout.write(`Failed to set date input time: ${time}${EOL}`);
-    process.exitCode = 1;
     return;
 }
 
