2a583ee73ca6c3d25702023568af6401b9afb55c
Dylan <dylan.langston@gmail.com>
2024-01-25 10:20:29 -0500

    Debug in main instead of post script

--- .github/actions/PopulateBuildSummary/main.mjs
+++ .github/actions/PopulateBuildSummary/main.mjs
@@ -3,3 +3,17 @@ import { validations } from './validations.mjs';
 const mode = process.env.INPUT_MODE.toLowerCase();
 
 validations.validateMode(mode);
+
+const runId = process.env.INPUT_RUNID;
+const startTime = "Unknown";
+
+try {
+    const workFlowRun = await js_common.getWorkflowDetails(runId);
+    console.log(workFlowRun);
+    js_common.notice(workFlowRun);
+    startTime = JSON.parse(workFlowRun).run_started_at;
+}
+catch (err) {
+    console.log(err);
+    js_common.notice(`Failed to fetch workflow run details: ${err}`);
+}
