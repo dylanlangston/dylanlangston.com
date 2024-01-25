a9d1e57142d19c4bc9e784591d7a4bcf335a25c6
Dylan Langston <dylanlangston@gmail.com>
2024-01-25 18:36:40 -0500

    Refactor PopulateBuildSummary post.mjs

--- .github/actions/PopulateBuildSummary/post.mjs
+++ .github/actions/PopulateBuildSummary/post.mjs
@@ -1,8 +1,8 @@
 import { summary } from './summary.mjs';
-import { validations } from './validations.mjs';
 import { js_common } from '../js_common.mjs';
 
 // Start Main
+const mode = process.env.INPUT_MODE.toLowerCase();
 switch (mode) {
     case "onpush":
     case "automerge":
@@ -23,45 +23,35 @@ switch (mode) {
         js_common.error(`Invalid Operation!`);
 };
 
-// Inputs
-let version = process.env.INPUT_VERSION;
-const mode = process.env.INPUT_MODE.toLowerCase();
-const runId = process.env.INPUT_RUNID;
-let startTime = "Unknown";
-try {
-    const workflowJobs = JSON.parse(await js_common.getWorkflowJobsDetails(runId));
-    const currentJob = workflowJobs.jobs.filter(j => j.status == "in_progress")[0];
-    startTime = currentJob.started_at;
-}
-catch (err) {
-    js_common.error(`Failed to fetch workflow run details: ${err}`);
-}
-
-// Validations
-if (!validations.versionValid(version)) {
-    version = "Unknown"
-}
-else {
-    version = `v${version}`;
-}
-validations.validateMode(mode);
-
-function Test() {
-    summary.addRaw(`* Started at ${startTime}.`, true);
-    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
-}
-function Build() {
-    summary.addRaw(`* Started at ${startTime}.`, true);
-    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
-}
-function Deploy() {
-    summary.addRaw(`* Started at ${startTime}.`, true);
-    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
-}
-function Release() {
-    summary.addRaw(`* Started at ${startTime}.`, true);
-    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
-}
-
 // Write the summary
 summary.write({overwrite: false});
+
+async function getStartTime() {
+    const runId = process.env.INPUT_RUNID;
+    let startTime = "Unknown";
+    try {
+        const workflowJobs = JSON.parse(await js_common.getWorkflowJobsDetails(runId));
+        const currentJob = workflowJobs.jobs.filter(j => j.status == "in_progress")[0];
+        startTime = currentJob.started_at;
+    }
+    catch (err) {
+        js_common.error(`Failed to fetch workflow run details: ${err}`);
+    }
+}
+
+async function Test() {
+    summary.addRaw(`* Started at ${await getStartTime()}.`, true);
+    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
+}
+async function Build() {
+    summary.addRaw(`* Started at ${await getStartTime()}.`, true);
+    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
+}
+async function Deploy() {
+    summary.addRaw(`* Started at ${await getStartTime()}.`, true);
+    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
+}
+async function Release() {
+    summary.addRaw(`* Started at ${await getStartTime()}.`, true);
+    summary.addRaw(`* Completed at ${new Date().toISOString()}.`, true);
+}
