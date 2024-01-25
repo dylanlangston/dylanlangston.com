4b8b2cc454b1c0cd60e4b76726de02fb1e5f2b08
Dylan <dylan.langston@gmail.com>
2024-01-25 10:27:37 -0500

    Remove debugging code

--- .github/actions/PopulateBuildSummary/main.mjs
+++ .github/actions/PopulateBuildSummary/main.mjs
@@ -1,20 +1,5 @@
 import { validations } from './validations.mjs';
-import { js_common } from '../js_common.mjs';
 
 const mode = process.env.INPUT_MODE.toLowerCase();
 
 validations.validateMode(mode);
-
-const runId = process.env.INPUT_RUNID;
-const startTime = "Unknown";
-
-try {
-    const workFlowRun = await js_common.getWorkflowDetails(runId);
-    console.log(workFlowRun);
-    js_common.notice(workFlowRun);
-    startTime = JSON.parse(workFlowRun).run_started_at;
-}
-catch (err) {
-    console.log(err);
-    js_common.notice(`Failed to fetch workflow run details: ${err}`);
-}
--- .github/actions/PopulateBuildSummary/post.mjs
+++ .github/actions/PopulateBuildSummary/post.mjs
@@ -6,17 +6,13 @@ import { js_common } from '../js_common.mjs';
 let version = process.env.INPUT_VERSION;
 const mode = process.env.INPUT_MODE.toLowerCase();
 const runId = process.env.INPUT_RUNID;
-const startTime = "Unknown";
-
+let startTime = "Unknown";
 try {
     const workFlowRun = await js_common.getWorkflowDetails(runId);
-    console.log(workFlowRun);
-    js_common.notice(workFlowRun);
     startTime = JSON.parse(workFlowRun).run_started_at;
 }
 catch (err) {
-    console.log(err);
-    js_common.notice(`Failed to fetch workflow run details: ${err}`);
+    js_common.error(`Failed to fetch workflow run details: ${err}`);
 }
 
 // Validations
