a1e56a1bad5ca5a12df022ef66e17051454c879b
Dylan <dylan.langston@gmail.com>
2024-01-25 10:18:10 -0500

    More debug logging

--- .github/actions/PopulateBuildSummary/post.mjs
+++ .github/actions/PopulateBuildSummary/post.mjs
@@ -7,11 +7,16 @@ let version = process.env.INPUT_VERSION;
 const mode = process.env.INPUT_MODE.toLowerCase();
 const runId = process.env.INPUT_RUNID;
 const startTime = "Unknown";
+
 try {
-    startTime = JSON.parse(await js_common.getWorkflowDetails(runId)).run_started_at;
+    const workFlowRun = await js_common.getWorkflowDetails(runId);
+    console.log(workFlowRun);
+    js_common.notice(workFlowRun);
+    startTime = JSON.parse(workFlowRun).run_started_at;
 }
 catch (err) {
-    js_common.error(`Failed to fetch start time: ${err}`);
+    console.log(err);
+    js_common.notice(`Failed to fetch workflow run details: ${err}`);
 }
 
 // Validations
