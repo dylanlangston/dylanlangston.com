2ff4093c7f5b62e8ea825a837002020d27f2a4e4
Dylan <dylan.langston@gmail.com>
2024-01-31 16:36:41 -0500

    Adjust version string padding so numbers won't overlap

--- .github/actions/VersionString/main.mjs
+++ .github/actions/VersionString/main.mjs
@@ -3,16 +3,20 @@ import { js_common } from '../js_common.mjs';
 const time = process.env.INPUT_TIME;
 const date = new Date(time);
 
+function pad(value) {
+    return new String(value).padStart(2, "0")
+}
+
 const major = 1;
-const minor = parseInt(`${date.getFullYear() - 2000}${date.getMonth()}${date.getDate()}`);
-const patch = parseInt(`${date.getHours()}${date.getMinutes()}${date.getSeconds()}`);
+const minor = parseInt(`${pad(date.getFullYear() - 2000)}${pad(date.getMonth())}${pad(date.getDate())}`);
+const patch = parseInt(`${pad(date.getHours())}${pad(date.getMinutes())}${pad(date.getSeconds())}`);
 
 if (isNaN(minor) || isNaN(patch)) 
 {
     js_common.error(`Failed to set date from input "time": ${time}`)
 }
 
-const version = `${major}.${new String(minor).padEnd(6, "0")}.${new String(patch).padEnd(6, "0")}`;
+const version = `${major}.${minor}.${patch}`;
 
 js_common.output('version', version);
 js_common.notice(`Version: ${version}`);
