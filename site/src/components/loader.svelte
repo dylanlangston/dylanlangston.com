37790a1f921322fa4e36594f4f592748946e1548
Dylan <dylanlangston@gmail.com>
2024-02-16 15:55:42 +0000

    Improved loading animation

--- site/src/components/loader.svelte
+++ site/src/components/loader.svelte
@@ -10,7 +10,7 @@
 		animation:
 			box-animation 1s infinite linear alternate,
 			rotate-animation 2s infinite ease,
-			shadow-animation 3s infinite alternate,
+			shadow-animation 9s infinite alternate,
       fade-in 2s;
 	}
 	@keyframes box-animation {
@@ -25,10 +25,10 @@
 		60%,
 		100% {
 			box-shadow:
-				5vw 0 0 2vw,
-				5vw 0 0 2vw,
-				-5vw 0 0 2vw,
-				-5vw 0 0 2vw;
+				5vw 0 0 3vw,
+				5vw 0 0 3vw,
+				-5vw 0 0 3vw,
+				-5vw 0 0 3vw;
 		}
 	}
 
@@ -44,11 +44,26 @@
 	}
 
 	@keyframes shadow-animation {
-		0%{
-			filter: hue-rotate(180deg) drop-shadow(0px 0px 3vh black);
+		0% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 3vh red);
 		}
-		50% {
-			filter: hue-rotate(180deg) drop-shadow(0px 0px 5vh white);
+		14.3% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 5vh orange);
+		}
+		28.6% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 3vh yellow);
+		}
+		42.9% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 5vh green);
+		}
+		57.2% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 3vh blue);
+		}
+		71.5% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 5vh purple);
+		}
+		85.8% {
+			filter: hue-rotate(180deg) drop-shadow(0px 0px 3vh black);
 		}
 	}
 
