7ade63f524666909614d47541bd0daf3cb048b72
Dylan <dylanlangston@gmail.com>
2024-03-01 17:08:40 +0000

    Update cache when setup-build.sh changes

--- setup-build.sh
+++ setup-build.sh
@@ -5,16 +5,6 @@
 # Update and Install Deps
 apt-get update && apt-get -y install --no-install-recommends ca-certificates bash curl unzip xz-utils make git python3
 
-# Install rust
-curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | bash
-
-# Install Cargo B(inary)Install
-curl -L --proto '=https' --tlsv1.3 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-# Install Cargo Lambda
-source "$HOME/.cargo/env"
-cargo binstall cargo-lambda
-
 # Install ZVM - https://github.com/tristanisham/zvm
 cur -L --proto '=https' --tlsv1.3 -sSf https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
 echo "# ZVM" >> $HOME/.bashrc
@@ -27,6 +17,15 @@ $HOME/.zvm/self/zvm i master
 
 # Install ZLS
 $HOME/.zvm/self/zvm i -D=zls master
+
+# Install rust
+curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | bash
+
+# Install Cargo B(inary)Install
+curl -L --proto '=https' --tlsv1.3 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+# Install Cargo Lambda
+$HOME/.cargo/bin/cargo binstall cargo-lambda
 
 # Install Node
 apt-get -y install --no-install-recommends nodejs npm
