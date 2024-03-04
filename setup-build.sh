8658e4d486ad86a84a74c8619d625a2e00b9445a
Dylan <dylanlangston@gmail.com>
2024-03-04 15:30:20 +0000

    Fix setup-build.sh

--- setup-build.sh
+++ setup-build.sh
@@ -5,6 +5,27 @@
 # Update and Install Deps
 apt-get update && apt-get -y install --no-install-recommends ca-certificates bash curl unzip xz-utils make git python3
 
+# Make Setup
+make clean-cache setup USE_NODE=1
+
+# Install Node
+apt-get -y install --no-install-recommends nodejs npm
+
+# Clean temp files
+apt-get clean && rm -rf /var/cache/apt/* && rm -rf /var/lib/apt/lists/* && rm -rf /tmp/*
+
+# Install Bun
+#curl -fsSL https://bun.sh/install | bash
+
+# Install rust
+curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | bash -s -- -y
+
+# Install Cargo B(inary)Install
+curl -L --proto '=https' --tlsv1.3 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+# Install Cargo Lambda
+$HOME/.cargo/bin/cargo binstall cargo-lambda -y
+
 # Install ZVM - https://github.com/tristanisham/zvm
 cur -L --proto '=https' --tlsv1.3 -sSf https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
 echo "# ZVM" >> $HOME/.bashrc
@@ -18,22 +39,9 @@ $HOME/.zvm/self/zvm i master
 # Install ZLS
 $HOME/.zvm/self/zvm i -D=zls master
 
-# Install rust
-curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | bash -s -- -y
+# Update PATH
+. $HOME/.bashrc
 
-# Install Cargo B(inary)Install
-curl -L --proto '=https' --tlsv1.3 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-# Install Cargo Lambda
-$HOME/.cargo/bin/cargo binstall cargo-lambda -y
-
-# Install Node
-apt-get -y install --no-install-recommends nodejs npm
-
-# Install Bun
-#curl -fsSL https://bun.sh/install | bash
-
-# Setup
-make clean-cache setup USE_NODE=1
-
-apt-get clean && rm -rf /var/cache/apt/* && rm -rf /var/lib/apt/lists/* && rm -rf /tmp/*
+# Check that path was updated
+zig version
+cargo version
