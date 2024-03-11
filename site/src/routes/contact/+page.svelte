03634f84169557f0187149faa01a43298b67c2f1
Dylan Langston <dylanlangston@gmail.com>
2024-03-11 00:13:34 -0400

    Getting closer to contact page working

--- site/src/routes/contact/+page.svelte
+++ site/src/routes/contact/+page.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
 	import Panel from '../../components/panel.svelte';
 	import Ripple from '../../components/ripple.svelte';
-	import { Environment } from '$lib/Common';
+
+	let firstName = '';
+	let lastName = '';
+	let email = '';
+	let phone = '';
+	let message = '';
+
+	async function handleSubmit(event) {
+		alert('submit');
+
+		event.preventDefault();
+
+		const formData = {
+			firstName,
+			lastName,
+			email,
+			phone,
+			message
+		};
+
+		try {
+			const response = await fetch('https://api.dylanlangston.com/contact', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(formData)
+			});
+
+			if (response.ok) {
+				alert('Message sent successfully!');
+			} else {
+				alert('Failed to send message. Please try again later.');
+				console.error(response.statusText);
+			}
+		} catch (error) {
+			alert('Failed to send message. Please try again later.');
+			console.error(error);
+		}
+	}
 </script>
 
 <Panel>
@@ -10,13 +49,14 @@
 			<p class="text-3xl font-semibold sm:text-4xl">Get in touch</p>
 		</div>
 		<div class="mt-6 max-w-lg mx-5">
-			<form onsubmit="event.preventDefault()" class="space-y-5">
+			<form on:submit={handleSubmit} class="space-y-5">
 				<div class="flex flex-col items-center gap-y-5 gap-x-6 [&>*]:w-full sm:flex-row">
 					<div>
 						<label class="font-medium"> First name </label>
 						<input
 							type="text"
 							required
+							bind:value={firstName}
 							class="w-full mt-2 px-3 py-2 bg-transparent outline-none border border-black dark:border-white shadow-sm rounded-lg"
 						/>
 					</div>
@@ -25,6 +65,7 @@
 						<input
 							type="text"
 							required
+							bind:value={lastName}
 							class="w-full mt-2 px-3 py-2 bg-transparent outline-none border border-black dark:border-white shadow-sm rounded-lg"
 						/>
 					</div>
@@ -34,6 +75,7 @@
 					<input
 						type="email"
 						required
+						bind:value={email}
 						class="w-full mt-2 px-3 py-2 bg-transparent outline-none border border-black dark:border-white shadow-sm rounded-lg"
 					/>
 				</div>
@@ -43,6 +85,7 @@
 						type="tel"
 						placeholder="+1 234-567-8901"
 						pattern="\+?([0-9]{1}[- ])?[0-9]{3}[- ][0-9]{3}[- ][0-9]{4}"
+						bind:value={phone}
 						class="w-full mt-2 px-3 py-2 bg-transparent outline-none border border-black dark:border-white shadow-sm rounded-lg placeholder:text-black/[.5]"
 					/>
 				</div>
@@ -50,16 +93,18 @@
 					<label class="font-medium"> Message </label>
 					<textarea
 						required
+						bind:value={message}
 						class="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border border-black dark:border-white shadow-sm rounded-lg"
 					></textarea>
 				</div>
-					<Ripple color={'currentColor'}>
-						<button
-							class="w-full px-4 py-2 font-medium rounded-lg duration-150 hover:shadow-md hover:bg-rainbow transition-colors"
-						>
-							Submit
-						</button>
-					</Ripple>
+				<Ripple color={'currentColor'}>
+					<button
+						type="submit"
+						class="w-full px-4 py-2 font-medium rounded-lg duration-150 hover:shadow-md hover:bg-rainbow transition-colors"
+					>
+						Submit
+					</button>
+				</Ripple>
 			</form>
 		</div>
 	</svelte:fragment>
