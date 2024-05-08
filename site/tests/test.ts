b0c6540debf65ed99c2a0bff53e9f636124ebb10
Dylan <dylanlangston@gmail.com>
2024-05-08 14:37:45 +0000

    Add test to check that raylib loaded correctly in integration tests

--- site/tests/test.ts
+++ site/tests/test.ts
@@ -1,8 +1,8 @@
-import { expect, test } from '@playwright/test';
+import { expect, test, type Page, type TestInfo, type ConsoleMessage } from '@playwright/test';
 
 test('index page has expected layout', async ({ page }, testinfo) => {
 	await page.goto('/');
-	page.waitForLoadState("domcontentloaded");
+	await page.waitForLoadState("domcontentloaded");
 
 	await expect(page.getByRole('heading', { name: "I'm Dylan Langston" })).toBeVisible({
 		timeout: 5000
@@ -12,4 +12,26 @@ test('index page has expected layout', async ({ page }, testinfo) => {
 		fullPage: true,
 		stylePath: ['tests/background-mask.css']
 	});
+});
+
+test('raylib loaded successfully', async ({ page }, testInfo: TestInfo) => {
+	const waitForConsoleMessage = async (page: Page, searchString: string, timeout: number): Promise<ConsoleMessage> =>
+		new Promise<ConsoleMessage>((resolve, reject) => {
+			const timeoutId = setTimeout(() => {
+				reject(new Error(`Timed out waiting for console message containing "${searchString}"`));
+			}, timeout);
+
+			page.on('console', async (msg: ConsoleMessage) => {
+				const messageText: string = msg.text();
+				if (messageText.includes(searchString)) {
+					clearTimeout(timeoutId);
+					resolve(msg);
+				}
+			});
+		});
+
+	const consoleMsgPromise = waitForConsoleMessage(page, 'TRACE: Raylib Started', 5000);
+	await page.goto('/');
+	await page.waitForLoadState("domcontentloaded");
+	const consoleMessage = await consoleMsgPromise;
 });
