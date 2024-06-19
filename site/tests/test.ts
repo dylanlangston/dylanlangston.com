533ebea915209edfd4aef8cfe2e27a403eb18b34
Dylan Langston <mail@dylanlangston.com>
2024-06-19 22:34:30 +0000

    Fix integration tests

--- site/tests/test.ts
+++ site/tests/test.ts
@@ -1,6 +1,10 @@
 import { expect, test, type Page, type TestInfo, type ConsoleMessage } from '@playwright/test';
 
 test('index page has expected layout', async ({ page }, testinfo) => {
+	await page.addInitScript({
+		content: 'document.cookie = "cookie_preferences=%7B%22analytics%22%3Atrue%2C%22userAccepted%22%3Atrue%7D"'
+	});
+
 	await page.goto('/');
 	await page.waitForLoadState("domcontentloaded");
 
@@ -20,6 +24,10 @@ test('raylib loaded successfully', async ({ page, browser }, testInfo: TestInfo)
 		testInfo.skip(true, "Not supported in non-chromium browsers due to webgl limitations");
 		return;
 	}
+
+	await page.addInitScript({
+		content: 'document.cookie = "cookie_preferences=%7B%22analytics%22%3Atrue%2C%22userAccepted%22%3Atrue%7D"'
+	});
 
 	const waitForConsoleMessage = async (page: Page, searchString: string, timeout: number): Promise<ConsoleMessage> =>
 		new Promise<ConsoleMessage>((resolve, reject) => {
