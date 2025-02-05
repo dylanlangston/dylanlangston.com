7ecb021347f523fe8797f9c080717ce2139f5d6b
Dylan Langston <mail@dylanlangston.com>
2025-02-05 04:28:36 +0000

    Fetch Resume as part of website build

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { resolve, join } from 'path';
 import optimizeWASMPlugin from './OptimizeWASMPlugin.ts';
+import fetchJSONResume from './FetchJSONResume.ts'
 import { getArgs } from './svelte.config.js';
 import tailwindcss from '@tailwindcss/vite'
 const args = getArgs();
@@ -10,6 +11,10 @@ const args = getArgs();
 export default defineConfig({
 	plugins: [
 		optimizeWASMPlugin({enabled: !args.Debug}),
+		fetchJSONResume({
+			url: 'https://gist.githubusercontent.com/dylanlangston/80380ec68b970189450dd2fae4502ff1/raw/resume.json',
+			filename: 'resume.json',
+		}),
 		tailwindcss(),
 		sveltekit()
 	],
